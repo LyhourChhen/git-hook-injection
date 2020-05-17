8c478133ae01543a6040e65bbf69999d098d80c6
LyhourChhen <ukamilove66@gmail.com>
2020-05-17 16:33:29 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,28 @@
 const { Telegraf } = require('telegraf')
 const message = "The repo has been change and merge please make pull request"
-const sendToGroup = () => {
-    bot.telegram.sendMessage(-405150540, message)
+
+const groupId="YOUR GROUP ID"
+const telegramBotToken="Your TOKEN"
+
+const getGroupId = ()=> {
+    bot.on("text", ctx => console.log("This is your id", ctx.chat.id))
 }
-const bot = new Telegraf("1206032150:AAGwkvJyNePiUWMcTz6RbiAAyxMdJM-9M84")
+
+const sendToGroup = () => {
+    bot.telegram.sendMessage(groupId, message)
+}
+const bot = new Telegraf(telegramBotToken)
 sendToGroup()
+// getGroupId();
+// GET GROUP ID 
+// Talk with botFater to get a new bot 
+// GET BOT TOKEN into bot-script 
+// Add bot into group that we want to notify 
+// Uncomment it only when you want to get groupId 
+// After Add into group start the bot by `navigate to .git-hook npm or yarn start but don't forget to comment sendToGroup()`
+// Then you will see the groupID in your console after just add as static into groupId varieble.
+// Uncomment sendToGroup() function and commnet back bot.on
+// and it should be work :)
 bot.launch()
 setTimeout(() => {
     process.exit()
